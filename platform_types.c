23b2e7f6edf47cdc2e08218913b28dfdbf6171a8
mohmdhtm70 <109278155+mohmdhtm70@users.noreply.github.com>
2023-02-08 23:40:42 +0200

    Add files via upload

---
+++ platform_types.c
@@ -0,0 +1,52 @@
+/*
+# * platform_types.c
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: muhammed hatem
+ */
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+typedef char               int8_t;
+typedef unsigned char      uint8_t;
+
+typedef short              int16_t;
+typedef unsigned short     uint16_t;
+
+typedef int                int32_t;
+typedef unsigned int       uint32_t;
+
+typedef long long          int64_t;
+typedef unsigned long long uint64_t;
+
+typedef float              float32_t;
+typedef unsigned float     ufloat32_t;
+
+typedef double              float64_t;
+typedef unsigned double     ufloat64_t;
+
+typedef volatile int8_t      vint8_t;
+typedef volatile uint8_t      vuint8_t;
+
+typedef volatile int16_t      vint16_t;
+typedef volatile uint16_t     vuint16_t;
+
+typedef volatile int32_t      vint32_t;
+typedef volatile uint32_t      vuint32_t;
+
+typedef volatile int64_t       vint64_t;
+typedef volatile uint64_t       vuint64_t;
+
+typedef volatile  float32_t     vfloat32_t;
+typedef volatile  ufloat32_t     vufloat32_t;
+
+typedef volatile  float64_t     vfloat64_t;
+typedef volatile  ufloat64_t     vufloat64_t;
+
+#endif
+
+
+
+
+
+
